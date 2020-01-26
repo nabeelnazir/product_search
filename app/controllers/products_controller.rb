9398f1945fb6949b51056926a3b00416b6c52224
nabeelnazir <nabeeln796@yahoo.com>
2020-01-26 16:48:34 +0500

    code refactored with meaningful methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,32 +1,62 @@
 class ProductsController < ApplicationController
   def index
-    search = Sunspot.search(Product) do
-      fulltext params[:search] do
-        fields(:description, :country, :tags, :title)
-        fields(:description, :country, :tags, :title => 2.0) if params[:sort_order]&.eql?("Relevance")
+    begin
+      search = Sunspot.search(Product) do
+        fulltext search_params[:search] do
+          fields(:description, :country, :tags, :title)
+          fields(:description, :country, :tags, :title => 2.0) if sort_order("Relevance")
+        end
+        with :country, search_params[:country] if country?
+        with :price, search_params[:price] if price_and_price_type("Equal To")
+        with(:price).less_than(search_params[:price].to_i) if price_and_price_type("Less Than")
+        with(:price).greater_than(search_params[:price].to_i) if price_and_price_type("Greater Than")
+        order_by :created_at, :desc if sort_order("Newest")
+        order_by :price, :asc if sort_order("Lowest Price")
+        order_by :price, :desc if sort_order("Highest Price")
+        paginate(:page => search_params[:page], :per_page => search_params[:per_page] || 5)
       end
-      with :country, params[:country] if params[:country].present?
-      with :price, params[:price] if params[:price].present? && params[:price_type]&.eql?("Equal To")
-      with(:price).less_than(params[:price].to_i) if params[:price].present? && params[:price_type]&.eql?("Less Than")
-      with(:price).greater_than(params[:price].to_i) if params[:price].present? && params[:price_type]&.eql?("Greater Than")
-      order_by :created_at, :desc if params[:sort_order]&.eql?("Newest")
-      order_by :price, :asc if params[:sort_order]&.eql?("Lowest Price")
-      order_by :price, :desc if params[:sort_order]&.eql?("Highest Price")
-      paginate(:page => params[:page], :per_page => params[:per_page] || 5)
-    end
-    @products = search.results
-    respond_to do |format|
-      format.html
-      format.json { render json: @products }
+      @products = search.results
+      respond_to do |format|
+        format.html
+        format.json { render json: @products }
+      end
+    rescue => exception
+      flash[:error] = "Something went wrong! #{exception}"
     end
   end
 
   def show
-    @product = Product.find_by_id(params[:id])
+    @product = Product.find_by_id(search_params[:id])
     respond_to do |format|
       format.html
       format.js
     end
+  end
+
+  private
+
+  def search_params
+    params.permit(:id, :search, :country, :price, :sort_order, :price_type, :per_page, :page)
+  end
+
+  def country?
+    search_params[:country].present?
+  end
+
+  def price?
+    search_params[:price].present?
+  end
+
+  def sort_order(value)
+    search_params[:sort_order].eql?(value)
+  end
+
+  def price_type(value)
+    search_params[:price_type].eql?(value)
+  end
+
+  def price_and_price_type(value)
+    price? && price_type(value)
   end
 
 end
